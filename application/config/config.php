e107859cb063d393c7d6c0ad3fe4d3d326a8a07e
aftab7604 <a_aftab.s@hotmail.com>
2023-10-02 16:34:55 +0500

    live

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-//  $config['base_url'] = 'https://godzillamoney.com/';
- $config['base_url'] = 'http://localhost/gzm/';
+ $config['base_url'] = 'https://godzillamoney.com/';
+//  $config['base_url'] = 'http://localhost/gzm/';
 //  $config['base_url'] = 'https://test.godzillamoney.com/';
 
 
